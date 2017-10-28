25820d3c5cff83040589e46dfa72c2d219b647f5
Yuri <SeriniTY320@gmail.com>
2017-10-29 01:58:13 +0300

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -5,5 +5,5 @@ LOCAL_MODULE_CLASS = SHARED_LIBRARIES
 LOCAL_MODULE_SUFFIX = .so
 LOCAL_MULTILIB = both
 LOCAL_SRC_FILES_32 = vendor/lib/libdpframework.so
-LOCAL_SRC_FILES_64 = vendor/lib/libdpframework.so
+LOCAL_SRC_FILES_64 = vendor/lib64/libdpframework.so
 include $(BUILD_PREBUILT)
