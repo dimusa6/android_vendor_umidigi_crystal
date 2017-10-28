ba3caa0eec648aaf0e59ff1680e48520243a98c0
Yuri <SeriniTY320@gmail.com>
2017-10-29 02:02:34 +0300

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -1,9 +1,9 @@
 LOCAL_PATH := $(call my-dir)
-include $(CLEAR_VARS)
-LOCAL_MODULE = libdpframework
-LOCAL_MODULE_CLASS = SHARED_LIBRARIES
-LOCAL_MODULE_SUFFIX = .so
-LOCAL_MULTILIB = both
-LOCAL_SRC_FILES_32 = vendor/lib/libdpframework.so
-LOCAL_SRC_FILES_64 = vendor/lib64/libdpframework.so
-include $(BUILD_PREBUILT)
+#include $(CLEAR_VARS)
+#LOCAL_MODULE = libdpframework
+#LOCAL_MODULE_CLASS = SHARED_LIBRARIES
+#LOCAL_MODULE_SUFFIX = .so
+#LOCAL_MULTILIB = both
+#LOCAL_SRC_FILES_32 = vendor/lib/libdpframework.so
+#LOCAL_SRC_FILES_64 = vendor/lib64/libdpframework.so
+#include $(BUILD_PREBUILT)
