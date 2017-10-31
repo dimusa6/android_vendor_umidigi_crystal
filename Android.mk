63ca9b0fac818ed99b55b46794c7b42ff7ace7e5
SeriniTY320 <SeriniTY320@gmail.com>
2017-10-31 10:59:05 +0000

    Proprietary fixes

--- Android.mk
+++ Android.mk
@@ -1,9 +1,29 @@
 LOCAL_PATH := $(call my-dir)
-#include $(CLEAR_VARS)
-#LOCAL_MODULE = libdpframework
-#LOCAL_MODULE_CLASS = SHARED_LIBRARIES
-#LOCAL_MODULE_SUFFIX = .so
-#LOCAL_MULTILIB = both
-#LOCAL_SRC_FILES_32 = vendor/lib/libdpframework.so
-#LOCAL_SRC_FILES_64 = vendor/lib64/libdpframework.so
-#include $(BUILD_PREBUILT)
+include $(CLEAR_VARS)
+LOCAL_MODULE = libdpframework
+LOCAL_MODULE_CLASS = SHARED_LIBRARIES
+LOCAL_MODULE_SUFFIX = .so
+LOCAL_MULTILIB = both
+LOCAL_SRC_FILES_32 = proprietary/vendor/lib/libdpframework.so
+LOCAL_SRC_FILES_64 = proprietary/vendor/lib64/libdpframework.so
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := libaudiopolicymanager
+LOCAL_MODULE_OWNER := mediatek
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_SUFFIX := .so
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MULTILIB := 32
+LOCAL_SRC_FILES_32 := proprietary/lib/libaudiopolicymanager.so
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := libaudiopolicymanager
+LOCAL_MODULE_OWNER := mediatek
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_SUFFIX := .so
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MULTILIB := 64
+LOCAL_SRC_FILES_64 := proprietary/lib64/libaudiopolicymanager.so
+include $(BUILD_PREBUILT)
