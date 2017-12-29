85c57b7eeb135ce8551eb1b56779f3f56dc5d1ff
Diego Muñoz <dimusa.dms@gmail.com>
2017-12-29 04:08:54 +0100

    Fix error propietary
    Files can't copy to the ROM to work if you don't change it

--- crystal-vendor-blobs.mk
+++ crystal-vendor-blobs.mk
@@ -1,19 +1,23 @@
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/umidigi/crystal/bin,system/bin)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/umidigi/crystal/propietary/bin,system/bin)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/umidigi/crystal/etc,system/etc)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/umidigi/crystal/propietary/etc,system/etc)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/umidigi/crystal/lib,system/lib)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/umidigi/crystal/propietary/lib,system/lib)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/umidigi/crystal/priv-app,system/priv-app)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/umidigi/crystal/propietary/lib64,system/lib64)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/umidigi/crystal/usr,system/usr)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/umidigi/crystal/propietary/priv-app,system/priv-app)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/umidigi/crystal/vendor/bin,system/vendor/bin)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/umidigi/crystal/propietary/usr,system/usr)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/umidigi/crystal/vendor/etc,system/vendor/etc)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/umidigi/crystal/propietary/vendor/bin,system/vendor/bin)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/umidigi/crystal/vendor/firmware,system/vendor/firmware)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/umidigi/crystal/propietary/vendor/etc,system/vendor/etc)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/umidigi/crystal/vendor/lib,system/vendor/lib)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/umidigi/crystal/propietary/vendor/firmware,system/vendor/firmware)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/umidigi/crystal/vendor/thh,system/vendor/thh)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/umidigi/crystal/propietary/vendor/lib,system/vendor/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/umidigi/crystal/propietary/vendor/lib64,system/vendor/lib64)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/umidigi/crystal/propietary/vendor/thh,system/vendor/thh)
