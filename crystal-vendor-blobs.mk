bef91d73a1ccc429532abecf542cdc1403aa8049
Diego Muñoz <dimusa.dms@gmail.com>
2017-12-29 04:33:36 +0100

    Update crystal-vendor-blobs.mk

--- crystal-vendor-blobs.mk
+++ crystal-vendor-blobs.mk
@@ -1,23 +1,23 @@
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/umidigi/crystal/propietary/bin,system/bin)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/umidigi/crystal/proprietary/bin,system/bin)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/umidigi/crystal/propietary/etc,system/etc)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/umidigi/crystal/proprietary/etc,system/etc)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/umidigi/crystal/propietary/lib,system/lib)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/umidigi/crystal/proprietary/lib,system/lib)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/umidigi/crystal/propietary/lib64,system/lib64)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/umidigi/crystal/proprietary/lib64,system/lib64)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/umidigi/crystal/propietary/priv-app,system/priv-app)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/umidigi/crystal/proprietary/priv-app,system/priv-app)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/umidigi/crystal/propietary/usr,system/usr)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/umidigi/crystal/proprietary/usr,system/usr)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/umidigi/crystal/propietary/vendor/bin,system/vendor/bin)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/umidigi/crystal/proprietary/vendor/bin,system/vendor/bin)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/umidigi/crystal/propietary/vendor/etc,system/vendor/etc)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/umidigi/crystal/proprietary/vendor/etc,system/vendor/etc)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/umidigi/crystal/propietary/vendor/firmware,system/vendor/firmware)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/umidigi/crystal/proprietary/vendor/firmware,system/vendor/firmware)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/umidigi/crystal/propietary/vendor/lib,system/vendor/lib)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/umidigi/crystal/proprietary/vendor/lib,system/vendor/lib)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/umidigi/crystal/propietary/vendor/lib64,system/vendor/lib64)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/umidigi/crystal/proprietary/vendor/lib64,system/vendor/lib64)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/umidigi/crystal/propietary/vendor/thh,system/vendor/thh)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/umidigi/crystal/proprietary/vendor/thh,system/vendor/thh)
